8a37b2d6ae08925f58b61958dafd2204b306def7
RyotaIto <jojogeorge042@gmail.com>
2019-05-19 14:59:33 +0900

    [R.I] Uploaded 001

---
+++ Roulette_Project/Roulette/Assets/RouletteController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; //回転速度
+    System.Random r_num1 = new System.Random(1000);
+    System.Random r_num2 = new System.Random(1000);
+    int num1 = 0;
+    int num2 = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //マウスが押されたら回転速度を設定する
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.num1 = this.r_num1.Next(1000);
+            this.num2 = this.r_num2.Next(1000);
+            this.rotSpeed = num1 + num2;
+        }
+
+        //回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.96f;
+    }
+}
